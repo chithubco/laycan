90f29c6bd6362147d863e801a9804ff4593f2a33
SIRMIKKY <sirmikky@hotmail.com>
2018-07-05 21:05:32 +0100

    Made some minor changes on MackBook

--- database/seeds/NodeElementSeeder.php
+++ database/seeds/NodeElementSeeder.php
@@ -14,6 +14,7 @@ class NodeElementSeeder extends Seeder
     {
     	$faker = Faker\Factory::create();
     	
+        //Curtailment Package Node
         NodeElement::create([
         	'export_id' => $faker->numberBetween(1,100),
         	'node_id' => 1,
@@ -48,6 +49,184 @@ class NodeElementSeeder extends Seeder
         	'description' => $faker->sentence(),
         	'user_id' => 1,
         	'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //Nomination Letter Node
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 2,
+            'title' => 'Generate Nomination Letter',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 2,
+            'title' => 'Nomination Letter Signed',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 2,
+            'title' => 'Nomination Letter Send to NNPC',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 2,
+            'title' => 'Nomination Letter Send to DPR and Acknowledged',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //Clearances Recieved
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 3,
+            'title' => 'NNPC Clearnace Recieved',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 3,
+            'title' => 'DPR Clerance Recieved',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 3,
+            'title' => 'Clearances Recieved at Terminal',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //NXP Node
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 4,
+            'title' => 'NXP Form Filled and Send to Finance',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 4,
+            'title' => 'NXP Form Recieved from Finance',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 4,
+            'title' => 'NXP Sent to Terminal',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //Cargo DOcuments Node
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 5,
+            'title' => 'All Cargo Documents Recieved from Terminal',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 5,
+            'title' => 'Cargo Documents Filled in Right Folders',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 5,
+            'title' => 'Data and Records Updated',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //Outturn Verification
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 6,
+            'title' => 'Outturn Undertaken Letter Send to DPR',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 6,
+            'title' => 'FCPA for Outturn Activities Completed',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 6,
+            'title' => 'Visa Processing Completed',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 6,
+            'title' => 'Outturn Activities Completed',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //Loss Clain Activity Node
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 7,
+            'title' => 'Loss Claim Activities Completed',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
+        ]);
+
+        //CCI Activities Completed
+        NodeElement::create([
+            'export_id' => $faker->numberBetween(1,100),
+            'node_id' => 8,
+            'title' => 'CCI Activities Completed',
+            'description' => $faker->sentence(),
+            'user_id' => 1,
+            'status_id' => $faker->numberBetween(1,8),
         ]);
     }
 }
